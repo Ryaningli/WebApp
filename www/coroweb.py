540cdf88c5519e3328d0b0d504a02bcd835aebd8
ryan <1669971502@qq.com>
2021-03-03 21:25:31 +0800

    分手快乐

--- www/coroweb.py
+++ www/coroweb.py
@@ -1,8 +1,11 @@
+import asyncio
 import functools
 import inspect
+import os
 from urllib import parse
 from aiohttp import web
 from www.Log import MyLog
+from www.apis import APIError
 
 logger = MyLog.get_log()
 log = logger.get_logger()
@@ -150,3 +153,40 @@ class RequestHandler(object):
             return r
         except APIError as e:
             return dict(error=e.error, data=e.data, message=e.message)
+
+
+def add_static(app):
+     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+     app.router.add_static('/static/', path)
+     log.info('add static %s => %s' % ('/static/', path))
+
+
+# add_route函数，用来注册一个URL处理函数
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+        log.info('add route %s %s => %s(%s)' % (
+        method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+        app.router.add_route(method, path, RequestHandler(app, fn))
+
+
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app, fn)
